c889f900579db63e202f943f34ff9dbd642ec452
Muhammed Ayaan <littlepanda024@gmail.com>
2024-06-05 13:40:02 +0500

    .

--- src/componnets/Footer.jsx
+++ src/componnets/Footer.jsx
@@ -4,117 +4,117 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-<div className="bg-white py-10 px-6">
-    <div className="container mx-auto">
-        <h2 className="mb-10 text-3xl font-bold text-blue-600">
-          DON'T BE SHY <br /> TO SAY HI
-        </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-        <div>
-          <h3 className="font-semibold uppercase">Offices</h3>
-          <div className="mt-4">
-            <h4 className="font-medium">Dubai</h4>
-            <p>
-              Second Floor, Building 04
-              <br />
-              Business Bay.
-              <br />
-              +971 55 169 3129
-            </p>
+      <div className="bg-white py-10 px-6">
+        <div className="container mx-auto">
+          <h2 className="mb-10 text-3xl font-bold text-blue-600">
+            DON'T BE SHY <br /> TO SAY HI
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+            <div>
+              <h3 className="font-semibold uppercase">Offices</h3>
+              <div className="mt-4">
+                <h4 className="font-medium">Dubai</h4>
+                <p>
+                  Second Floor, Building 04
+                  <br />
+                  Business Bay.
+                  <br />
+                  +971 55 169 3129
+                </p>
+              </div>
+              <div className="mt-4">
+                <h4 className="font-medium">Canada</h4>
+                <p>
+                  Suit # 2505, 50 Ordnance Street,
+                  <br />
+                  Toronto, ON M6K1A2
+                  <br />
+                  +1 (437) 500-6267
+                </p>
+              </div>
+            </div>
+            <div>
+              <h3 className="font-semibold uppercase">Business</h3>
+              <div className="mt-4">
+                <h4 className="font-medium">Dubai</h4>
+                <a href="mailto:mohammad.zia@theakkaas.com" className="text-blue-600">
+                  mohammad.zia@theakkaas.com
+                </a>
+              </div>
+              <div className="mt-4">
+                <h4 className="font-medium">Canada</h4>
+                <a href="mailto:ali.zaidi@theakkaas.com" className="text-blue-600">
+                  ali.zaidi@theakkaas.com
+                </a>
+              </div>
+            </div>
+            <div>
+              <h3 className="font-semibold uppercase">Say Hi</h3>
+              <div className="mt-4">
+                <h4 className="font-medium">Contact</h4>
+                <a href="mailto:info@theakkaas.com" className="text-blue-600">
+                  info@theakkaas.com
+                </a>
+              </div>
+              <div className="mt-4">
+                <h4 className="font-medium">Jobs</h4>
+                <a href="mailto:jobs@theakkaas.com" className="text-blue-600">
+                  jobs@theakkaas.com
+                </a>
+              </div>
+            </div>
+            <div>
+              <h3 className="font-semibold uppercase">Contact</h3>
+              <div className="mt-4">
+                <p>
+                  Reach out using our cool{" "}
+                  <Link to="/contact" className="text-blue-600 font-bold">
+                    contact form
+                  </Link>
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="mt-4">
-            <h4 className="font-medium">Canada</h4>
-            <p>
-              Suit # 2505, 50 Ordnance Street,
-              <br />
-              Toronto, ON M6K1A2
-              <br />
-              +1 (437) 500-6267
-            </p>
-          </div>
-        </div>
-        <div>
-          <h3 className="font-semibold uppercase">Business</h3>
-          <div className="mt-4">
-            <h4 className="font-medium">Dubai</h4>
-            <a href="mailto:mohammad.zia@theakkaas.com" class="text-blue-600">
-              mohammad.zia@theakkaas.com
-            </a>
-          </div>
-          <div className="mt-4">
-            <h4 className="font-medium">Canada</h4>
-            <a href="mailto:ali.zaidi@theakkaas.com" class="text-blue-600">
-              ali.zaidi@theakkaas.com
-            </a>
-          </div>
-        </div>
-        <div>
-          <h3 className="font-semibold uppercase">Say Hi</h3>
-          <div className="mt-4">
-            <h4 className="font-medium">Contact</h4>
-            <a href="mailto:info@theakkaas.com" className="text-blue-600">
-              info@theakkaas.com
-            </a>
-          </div>
-          <div className="mt-4">
-            <h4 className="font-medium">Jobs</h4>
-            <a href="mailto:jobs@theakkaas.com" className="text-blue-600">
-              jobs@theakkaas.com
-            </a>
-          </div>
-        </div>
-        <div>
-          <h3 className="font-semibold uppercase">Contact</h3>
-          <div className="mt-4">
-            <p>
-              Reach out using our cool{" "}
-              <a href="#" className="text-blue-600 font-bold">
-                contact form
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="mt-10 flex justify-between flex-col md:flex-row">
-        <div className="flex md:flex-row flex-col gap-3 items-center">
-          <div>
-            <a href="#" className="text-blue-600">
-              <img
-                className="w-full max-w-[180px]"
-                src="https://theakkaas.com/wp-content/uploads/2023/01/theakkaas-logo-black.svg"
-                alt="Akkaas Logo"
-              />
-            </a>
-          </div>
-          <div className="flex space-x-2">
-            <a href="#" class="text-blue-600">
-              WHO WE ARE
-            </a>
-            <a href="#" class="text-blue-600">
-              SERVICES
-            </a>
-            <a href="#" class="text-blue-600">
-              PORTFOLIO
-            </a>
-          </div>
-        </div>
+          <div className="mt-10 flex justify-between flex-col md:flex-row">
+            <div className="flex md:flex-row flex-col gap-3 items-center">
+              <div>
+                <Link to="/" className="text-blue-600">
+                  <img
+                    src="https://theakkaas.com/wp-content/uploads/2023/01/theakkaas-logo-black.svg"
+                    alt="Akkaas Logo"
+                    className="w-full max-w-[180px]"
+                  />
+                </Link>
+              </div>
+              <div className="flex space-x-2">
+                <Link to="/who-we-are" className="text-blue-600">
+                  WHO WE ARE
+                </Link>
+                <Link to="/services" className="text-blue-600">
+                  SERVICES
+                </Link>
+                <Link to="/portfolio" className="text-blue-600">
+                  PORTFOLIO
+                </Link>
+              </div>
+            </div>
 
-        <div className="flex justify-center space-x-2">
-          <a href="#" className="text-blue-600">
-            Twitter
-          </a>
-          <a href="#" className="text-blue-600">
-            Instagram
-          </a>
-          <a href="#" className="text-blue-600">
-            LinkedIn
-          </a>
+            <div className="flex justify-center space-x-2">
+              <Link to="#" className="text-blue-600">
+                Twitter
+              </Link>
+              <Link to="#" className="text-blue-600">
+                Instagram
+              </Link>
+              <Link to="#" className="text-blue-600">
+                LinkedIn
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
     </footer>
-    
   );
 };
+
 export default Footer;
