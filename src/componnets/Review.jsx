218d7f7f7a8592aa45d8cf07409511baa3efce11
Muhammed Ayaan <littlepanda024@gmail.com>
2024-06-05 13:10:42 +0500

    .

--- src/componnets/Review.jsx
+++ src/componnets/Review.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Idk from "./Idk";
 const Review = () => {
   var settings = {
     dots: true,
@@ -144,7 +143,43 @@ const Review = () => {
         </div>
       </Slider>
     </section>
-    <Idk/>
+    <section>
+      <div>
+        <figure>
+          <img
+            className="w-full"
+            src="https://theakkaas.com/wp-content/uploads/2024/05/our-approach-bg.webp"
+            alt=""
+          />
+          <div className="absolute left-1/2 top-[383rem] transform -translate-x-1/2 -translate-y-1/2">
+            <figure className="flex flex-col items-center text-center">
+              <img
+                className="w-full max-w-[340px] h-auto"
+                src="https://theakkaas.com/wp-content/uploads/2023/01/theakkaas-studio-logo.svg"
+                alt=""
+              />
+            </figure>
+            <div className="flex flex-col items-center">
+              <div>
+                <h4 className="text-4xl font-normal font-sans text-white">
+                  OUR APPROACH TO
+                </h4>
+              </div>
+              <div>
+                <h4 className="text-4xl font-normal font-sans text-white/70">
+                  BUILDING THE WEB 3.0 BRANDS
+                </h4>
+              </div>
+              <div>
+                <h4 className="text-4xl font-normal font-sans text-white">
+                  OF TOMORROW
+                </h4>
+              </div>
+            </div>
+          </div>
+        </figure>
+      </div>
+    </section>
     </div>
   );
 };
